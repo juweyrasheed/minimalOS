134dcdf488fc064595da01200acc9f239a32201e
J2-HUSSAINRASH <juwairiya2.hussainrasheed@live.uwe.ac.uk>
2023-07-30 17:14:03 +0500

    New Line support and Scroll Function added

--- kernel.c
+++ kernel.c
@@ -61,7 +61,17 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
+
+void terminal_scroll() {
+  for (size_t y=0; y<VGA_HEIGHT - 1; y++) {
+	for (size_t x=0; x<VGA_WIDTH; x++) {
+	 const size_t index = y * VGA_WIDTH + x;
+	 const size_t new_index = (y+1) * VGA_WIDTH + x;
+	 terminal_buffer [index] = terminal_buffer[new_index];
+         }
+  }
+}
+ l
 void terminal_initialize() {
   terminal_row = 0;
   terminal_column = 0;
@@ -85,14 +95,30 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
-    terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT) {
-      terminal_row = 0;
-    }
-  }
+//Scrolling to a new line by checking if there is a character in a new line
+  if (c=='\n') {
+	++terminal_row;
+	terminal_colmn = 0;
+  if (terminal_row ==VGA_HEIGHT) {
+	terminal_scroll();
+	terminal_row--;
+	}
+   }
+
+//if the char is not in a new line
+else {
+terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column ==VGA_WIDTH){
+	 terminal_column = 0;
+	 if (++terminal_row == VGA_HEGHT) {
+	 terminal_scroll();
+	 terminal_row--;
+	 }
+	}
+      }
 }
+
+
  
 void terminal_writestring(const char* data) {
   size_t datalen = strlen(data);
@@ -111,5 +137,16 @@ void kernel_main() {
    * yet, '\n' will produce some VGA specific character instead.
    * This is normal.
    */
+  terminal_setcolor(COLOR_RED);
   terminal_writestring("Hello, kernel World!\n");
+ 
+  terminal_setcolor(COLOR_WHITE);
+  for (int i=0; i<6; i++) terminal_writestring("Hello, kernel World!\n");
+
+  terminal_setcolor(COLORBLUE);
+  for (int i=0; i<18; i++) terminal_writestring("Hello, kernel World!\n");
+  
+  terminal_setcolor(COLOR_GREEN);
+  for (int i=0; i<5; i++) terminal_writestring("Helo, kernel World!\n"); 
 }
+
